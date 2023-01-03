93d22bfc2af1982f5783c5478d522d5f838489ae
fmorelato <120057968+fmorelato@users.noreply.github.com>
2023-01-03 21:42:09 +0100

    Add files via upload

--- LFC/main/harmonic_oscillator/decovariance_time/correlation.c
+++ LFC/main/harmonic_oscillator/decovariance_time/correlation.c
@@ -13,28 +13,34 @@
 #include"random.h"
 #include"Markov.h"
 
-#define T_M 100000		/* number of sweeps after thermalization to compute gamma(t_M,t)  */
+#define T_M 50000		/* number of sweeps after thermalization to compute gamma(t_M,t)  */
 
-void inttochar(int n, char *a){
+void inttochar(int n, char *a)
+{
         int i,j;
         char v[4];
-        for(i=0; i<4; i++){
+        for(i=0; i<4; i++)
+	{
                 a[i] = '0';
         }
         sprintf(v,"%d",n);
-        for(i=1; i<4; i++){
-                if(v[i]=='\0'){
+        for(i=1; i<4; i++)
+	{
+                if(v[i]=='\0')
+		{
                         break;
                 }
         }
-        for(j=4-i; j<4; j++){
+        for(j=4-i; j<4; j++)
+	{
                 a[j]=v[j-4+i];
         }
         return;
 }
 
 
-int main(){
+int main()
+{
 	int i, j, k, acc=0;
 	int max_tm = 50;
 	FILE *gamma_doc;
@@ -44,38 +50,43 @@ int main(){
 	strcpy(filename,"data/gamma_data0000.txt");
 
 	correlat = (double**) malloc(N*sizeof(double*));
-	for(i=0; i<N; i++){
+	for(i=0; i<N; i++)
+	{
 		correlat[i] = (double*) malloc(T_M*sizeof(double));
-		for(j=0; j<T_M; j++){
-			correlat[i][j] = 0;
-		}
 	}
 
 	rlxd_init(1,251099);
 	ranlxd(xx, N);
-	for(i=0; i<N_therm; i++){	/* thermalization */
+	for(i=0; i<N_therm; i++)	/* thermalization */
+	{
 		sweep(&acc);
 	}
 
 		/*++++++++ write corr in a matrix at each Markov time  ++++++++*/
-	for(j=0; j<T_M; j++){
-		acc=0;
-		sweep(&acc);
+	printf("computing corr...\n");
+	for(j=0; j<T_M; j++)
+	{
+		int ac=0;
+		sweep(&ac);
+		acc+=ac;
 		corr(c);
-		for(i=0; i<N; i++){
-			if(c[i]>0){
-				correlat[i][j] = c[i];
-			}
+		for(i=0; i<N; i++)
+		{
+			correlat[i][j] = c[i];
 		}
+		/*printf("\r %2.2f %%", 100*(double)j/(double)T_M );*/
 	}
+	printf("%f\n", (float)acc/(N*T_M));
 
-
+	printf("\ncomputing gamma...\n");
 		/*++++++++ compute gamma(t_M,t)  ++++++++*/
-	for(i=0; i<N; i++){	/* i=t */
+	for(i=0; i<N; i++)	/* i=t */
+	{
 		double prod, meansq, gamma0;
 		char number[5];
 		inttochar(i,number);
-		for(j=0; j<4; j++){
+		for(j=0; j<4; j++)
+		{
 			filename[15+j]=number[j];
 		}
 		gamma_doc = fopen(filename, "wt");
@@ -84,7 +95,8 @@ int main(){
 		prod=.0;
 		meansq=.0;
 		gamma0=0;
-		for(j=0; j<T_M; j++){
+		for(j=0; j<T_M; j++)
+		{
 			prod += correlat[i][j]*correlat[i][j];
 			meansq += correlat[i][j];
 		}
@@ -96,28 +108,28 @@ int main(){
 		
 			/* gamma(j,t)/gamma(0,t) j>0 */
 		
-		for(j=1; j<max_tm; j++){	/* j=t_M */
+		for(j=1; j<max_tm; j++)	/* j=t_M */
+		{
 			double prod=.0;
 			double meansq=.0;
-			for(k=0; k<T_M-j; k++){
+			for(k=0; k<T_M-j; k++)
+			{
 				prod += correlat[i][k]*correlat[i][k+j];
 				meansq += correlat[i][k];
 			}
-			for(k=T_M-j; k<T_M; k++){
+			for(k=T_M-j; k<T_M; k++)
+			{
 				meansq += correlat[i][k];
 			}
 			prod/=(T_M-j);
 			meansq/=T_M;
 			meansq*=meansq;
 			fprintf( gamma_doc, "%d %f\n", j, (prod - meansq)/gamma0 );
-			/*if((prod-meansq)>0){
-				fprintf( gamma_doc, "%d %f\n", j, (prod - meansq)/gamma0 );
-			} else {
-				fprintf( gamma_doc, "%d %f\n", j, .0 );
-			}*/
 		}
+		free(correlat[i]);
 		fclose( gamma_doc );
 	}
+	free(correlat);
 
 	exit(EXIT_SUCCESS);	
 }
