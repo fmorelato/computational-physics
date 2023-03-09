cc45370744caac9992f6fb20e9d3dabb9cb6170b
fmorelato <120057968+fmorelato@users.noreply.github.com>
2023-03-09 22:16:08 +0100

    Add files via upload

---
+++ LFC/include/functions.h
@@ -0,0 +1,10 @@
+#ifndef FUNCTIONS_H
+#define FUNCTIONS_H
+
+#ifndef FUNCTIONS_C
+extern double my_arccosh(double x);
+extern double func_DE(double a, double b, double c);
+extern double func_M(double c, double DE, double t);
+#endif
+
+#endif
